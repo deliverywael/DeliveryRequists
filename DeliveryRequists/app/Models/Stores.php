ae14b725a88b086dcc92769574c77df3bfbf623a
WaelAlSyaidKhalil <waell.ite.sy@gmail.com>
2024-12-05 02:06:35 +0300

    UPD

--- DeliveryRequists/app/Models/Stores.php
+++ DeliveryRequists/app/Models/Stores.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Stores extends Model
 {
     use HasFactory;
+
+    protected $primaryKey = 'Store_id'; // تحديد مفتاح الجدول الرئيسي
+    protected $fillable = ['StoreCategory'];
+
+    public function products() {
+        return $this->hasMany(Product::class);
+    }
 }
